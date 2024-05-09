fba25c0da00537d9d829914fc3c27a5f6efdc683
shubhamurkude79 <shubhamurkude79@gmail.com>
2024-05-09 23:30:31 +0530

    Reactive form created for login and then ngrx/store was downgraded to supporting version of angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { CountryService } from './services/country.service';
 import { CountryListComponent } from './country-list/country-list.component';
 import { HomeComponent } from './home/home.component';
-import { AppRoutingModule } from './app-routing.module';
 import { CurrencyChartComponent } from './currency-chart/currency-chart.component';
 import { IndependentCountriesComponent } from './independent-countries/independent-countries.component';
 import { UnMemberChartComponent } from './un-member-chart/un-member-chart.component';
@@ -15,6 +18,9 @@ import { CountryListModule } from './country-list/country-list.module';
 import { PopulationChartComponent } from './population-chart/population-chart.component';
 import { ContinentChartComponent } from './continent-chart/continent-chart.component';
 import { LoginComponent } from './auth/pages/login/login.component';
+
+import { authReducer } from './auth/reducers/auth.reducers';
+import { AuthEffects } from './auth/effects/auth.effects';
 
 @NgModule({
   declarations: [
@@ -32,9 +38,12 @@ import { LoginComponent } from './auth/pages/login/login.component';
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     CountryListModule,
-    CountryListRoutingModule
+    CountryListRoutingModule,
+    StoreModule.forRoot({ auth: authReducer }),
+    EffectsModule.forRoot([AuthEffects])
   ],
   providers: [CountryService],
   bootstrap: [AppComponent]
--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { login } from '../../actions/auth.actions';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  loginForm: FormGroup;
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder, private store: Store) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if(this.loginForm.valid){
+      const {username, password} = this.loginForm.value;
+      this.store.dispatch(login({ username, password }));
+    }
   }
 
 }
