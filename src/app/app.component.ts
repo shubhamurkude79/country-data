5e9b8903168fc2ee8a915d5bae90980050d343ce
shubhamurkude79 <shubhamurkude79@gmail.com>
2024-05-09 23:55:02 +0530

    Login page configuration done in app-component but UI issue is there

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { CountryService } from './services/country.service';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { AuthState } from './auth/reducers/auth.reducers';
+import { logout } from './auth/actions/auth.actions';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +11,17 @@ import { CountryService } from './services/country.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  isAuthenticated$: Observable<boolean>;
+
   title = 'country-app';
-  constructor(private SidebarToggleService: CountryService) {}
+  constructor(private store: Store<{auth: AuthState}>, 
+    private SidebarToggleService: CountryService) {
+    this.isAuthenticated$ = this.store.select(state => state.auth.isAuthenticated);
+  }
+
+  logout() {
+    this.store.dispatch(logout());
+  }
 
   toggleSidebar() {
     this.SidebarToggleService.toggleSidebar();
