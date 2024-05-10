fb1eb47d75fc27b1c6e49786fbb94e3a2d75ed29
shubhamurkude79 <shubhamurkude79@gmail.com>
2024-05-10 20:23:18 +0530

    show hide typed password eye icon adjusted and styled

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -26,4 +26,9 @@ export class LoginComponent {
     }
   }
 
+  togglePasswordVisibility() {
+    const passwordInput = document.getElementById('password') as HTMLInputElement;
+    passwordInput.type = passwordInput.type === 'password' ? 'text' : 'password';
+  }
+
 }
