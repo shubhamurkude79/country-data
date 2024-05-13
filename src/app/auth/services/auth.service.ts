a790344e1d1791768f2bc7948ab89c2a9eb60e51
shubhamurkude79 <shubhamurkude79@gmail.com>
2024-05-13 21:59:02 +0530

    service file updated for register api call

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { environment } from "src/environments/environment";
-import { User } from "../models/auth.model";
+import { User, RegistrationData } from "../models/auth.model";
 
 const loginSysUrl = environment.loginSysUrl;
 
@@ -15,5 +15,10 @@ export class AuthService {
 
     login(username:string, password:string): Observable<{user:User, token:string}> {
         return this.http.post<{user:User, token:string}>(`${loginSysUrl}/login`, {username, password});
+    }
+
+    register(registrationData:RegistrationData): Observable<{registrationData:RegistrationData}> {
+        return this.http.post<{registrationData:RegistrationData, token: string}>(`${loginSysUrl}/register`, registrationData);
+
     }
 }
